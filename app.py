43102d33ae6af0a352988fe8f66af138a45ab5c4
maryllusco <maryluzllusco03@gmail.com>
2025-03-26 11:42:32 -0300

    Creamos links en flask

--- app.py
+++ app.py
@@ -3,5 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def principal():
+    return """
+      <a href='/hola'>hola</a>
+      <a href='/chau'>chau</a 
+      """
+
+@app.route("/hola")
+def saludar():
+    return "<h2>Hello, World!</h2>"
+
+@app.route("/chau")
+def despedir():
+    return "<h2>Chau!</h2>"
