3b8a3b73a9dd478c359e6c45521902a3a3b09b1c
Mary Llusco <maryluzllusco03@gmail.com>
2025-04-09 10:53:29 -0300

    agregamos codigo para agregar enlaces

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
-from flask import Flask
+from flask import Flask, url_for 
 
 app = Flask(__name__)
 
-@app.route("/")
-def principal():
-    return """
-      <a href='/hola'>hola</a>
-      <a href='/chau'>chau</a 
-      """
 
 @app.route("/hola")
 def saludar():
@@ -21,3 +15,26 @@ def despedir():
 def suma(n1, n2):
     suma = n1+n2
     return F"<h2>{n1} más {n2} es igual a {suma}</h2>"
+
+@app.route("/tirar-dado/<int:caras>")
+def dado (caras):
+    from random import randint
+    n = randint(1,caras)
+    return f"<p>Tire un dado de {caras} caras, salio {n}</p>"
+
+@app.route ("/")
+
+def main():
+    url_hola = url_for("saludar")
+    url_dado = url_for("dado",caras=6)
+    url_logo = url_for("static",filename="logo.png")
+
+    return f"""
+    <a href= "{url_hola}">Hola</a>
+    <br>
+    <a href= "{url_for("despedir")}">Chau</a>
+    <br>
+    <a href= "{url_logo}">Logo</a>
+    <br>
+    <a href= "{url_dado}">Tirar_dado</a>
+    """
