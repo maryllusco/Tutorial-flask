0e7171599f51ddf9327140c9c48e4f4929bb5957
Mary Llusco <maryluzllusco03@gmail.com>
2025-05-07 10:43:10 -0300

    agregue telefono y direccion

--- app.py
+++ app.py
@@ -101,15 +101,19 @@ def mostrar(id):
 def datos_plantilla(id):
    abrirConexion()
    cursor = db.cursor()
-   cursor.execute("SELECT id,usuario,email FROM usuarios WHERE id = ?;", (id,))
+   cursor.execute("SELECT id,usuario,email,telefono,direccion FROM usuarios WHERE id = ?;", (id,))
    res = cursor.fetchone()
    cerrarConexion()
    usuario = None
    email = None
+   telefono = None
+   direccion = None
    if res != None:
       usuario=res['usuario']
       email=res['email']
-   return render_template("datos.html", id=id, usuario=usuario, email=email)
+      telefono= res['telefono']
+      direccion=res['direccion']
+   return render_template("datos.html", id=id, usuario=usuario, email=email, telefono=telefono, direccion=direccion)
 
 
  
